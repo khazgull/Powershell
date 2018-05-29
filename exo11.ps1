a0b4fd882737cfbf405e98a55ab5d8f374f445b1
Alex <khazgull@hotmail.com>
2018-05-29 14:18:57 +0200

    correction

--- exo11.ps1
+++ exo11.ps1
@@ -2,3 +2,17 @@
 $test = Read-Host "donne moi un mot"
 $test.Length
 }
+
+function exo12
+{ 
+$test = Read-Host "donne moi un nombre"
+
+if ($test%2 -eq 1){
+
+
+    Write-Host "c'est un nombre impair"
+    }
+else {
+    Write-Host "c'est un nombre pair"}
+
+    }
